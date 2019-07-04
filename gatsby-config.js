1fa17ee5445cf344bc917c8223acc803a239475f
Oke <obinathaniel@gmail.com>
2019-07-04 01:18:22 +0100

    Initialize repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Find the best software developers in Africa`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
